dbae7b437ae8e37f87b70c35f34311df7be99905
Khanh Dao <jakedao1991@gmail.com>
2025-08-29 10:04:12 +0700

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.VITE_BASE_PATH || "/",
+  base: process.env.VITE_BASE_PATH || "https://project-one-piys.vercel.app/",
   plugins: [react()],
   css: {
     preprocessorOptions: {
